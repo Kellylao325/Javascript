c60d30cce0b2fe6a234623e253804ea9bfe49ef3
kellylao325 <kellylao@Kellys-Air.hsd1.il.comcast.net>
2019-08-27 16:33:41 -0500

    .

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select tbody
+var tbody = d3.select('tbody');
+
+//loop through data in dataset
+tableData.forEach ((item)=>{
+    var row = tbody.append('tr');
+    Object.entries(item).forEach(([key,value])=> {
+        var cell = row.append('td').text(value);
+        cell.text(value);
+    });
+});
+
+//filter button
+
+
+//complete click handler
+filterButton.on('click', function(){
+ 
+    //input element for filter
+    var inputDate = d3.select("#datetime");
+    var inputValue = inputDate.property("value");
+    var filterButton = d3.select('#filter-btn');
+    //prevent refreshing
+    d3.event.preventDefault();
+
+    //filtered table 
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+
+});
+
+
